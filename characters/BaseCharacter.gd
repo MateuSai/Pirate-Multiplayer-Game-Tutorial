33616a0983efef41f5b6c918c3fd7e942108d97f
MateuSai <mateu1502@gmail.com>
2022-07-06 20:15:43 +0200

    Part 1

---
+++ characters/BaseCharacter.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+class_name BaseCharacter
+
+var hp: int = 2 setget set_hp
+
+onready var sprite: Sprite = get_node("Sprite")
+
+
+func damage(dam: int) -> void:
+	self.hp -= dam
+	
+	
+func enable_hitbox() -> void:
+	pass
+	
+	
+func disable_hitbox() -> void:
+	pass
+
+
+func die() -> void:
+	pass
+
+
+func set_hp(new_hp: int) -> void:
+	hp = new_hp
+	if hp <= 0:
+		die()
---
+++ characters/Player.gd
@@ -0,0 +1,57 @@
+extends BaseCharacter
+
+const GRAVITY: int = 200
+const FRICTION: float = 0.1
+const ACCELERATION: float = 0.25
+
+var speed: int = 50
+var dir: float
+var jump_impulse: int = -85
+
+var velocity: Vector2 = Vector2.ZERO
+
+var can_move: bool = true setget set_can_move
+
+onready var state_machine: Node = get_node("StateMachine")
+onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
+onready var hitbox_col: CollisionShape2D = get_node("Hitbox/CollisionShape2D")
+
+
+func _get_input() -> void:
+	dir = Input.get_axis("ui_left", "ui_right")
+	if dir != 0:
+		if (dir > 0 and sprite.flip_h) or (dir < 0 and not sprite.flip_h):
+			_flip()
+		velocity.x = lerp(velocity.x, dir * speed, ACCELERATION)
+		
+	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
+		velocity.y = jump_impulse
+		
+		
+func _physics_process(delta: float) -> void:
+	if can_move:
+		_get_input()
+		
+	velocity.y += GRAVITY * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if dir == 0:
+		velocity.x = lerp(velocity.x, 0, FRICTION)
+
+
+func _flip() -> void:
+	sprite.flip_h = not sprite.flip_h
+	hitbox_col.position.x *= -1
+
+
+func enable_hitbox() -> void:
+	hitbox_col.disabled = false
+	
+	
+func disable_hitbox() -> void:
+	hitbox_col.disabled = true
+
+
+func set_can_move(new_value: bool) -> void:
+	can_move = new_value
+	if not can_move:
+		dir = 0.0
